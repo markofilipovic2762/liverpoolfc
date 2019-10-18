1984a33044e0f6f6aa8237f2210c583c34ed23b0
markofilipovic2762 <markofilipovicsd@gmail.com>
2019-10-18 13:24:35 +0200

    izmena 17.10

--- src/Components/Header_footer/Footer.js
+++ src/Components/Header_footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import {LfcLogo} from "../ui/icons"
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="bck_red">
+            <div className="footer_logo">
+                <LfcLogo 
+                    width="70px"
+                    height="70px"
+                    link={true}
+                    linkTo="/"
+                />
+            </div>
+            <div className="footer_discl">
+                Liverpool FC 2019. All rights reserved
+            </div>
+        </footer>
     );
 };
 
--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -24,10 +24,10 @@ const Header = () => {
                     </div>
                 </div>
                 <Link to="/the_team"> 
-                    <Button color="inherit"> The team </Button>
+                    <Button color="inherit">The team</Button>
                 </Link>
                 <Link to="/the_matches"> 
-                    <Button color="inherit"> Matches </Button>
+                    <Button color="inherit">Matches</Button>
                 </Link>
             </ToolBar>
             
