c69646dd2329f76b05417df9e73f9e126926c1ff
markofilipovic2762 <markofilipovicsd@gmail.com>
2019-11-09 12:15:54 +0100

    Add & edit players finished

---
+++ src/Components/admin/players/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import AdminLayout from '../../../HOC/AdminLayout';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { firebasePlayers } from '../../../firebase';
+import { firebaseLooper, reverseArray } from '../../ui/misc';
+import { red } from '@material-ui/core/colors';
+
+class AdminPlayers extends Component {
+
+    state = {
+        isLoading: true,
+        players: []
+    }
+
+    componentDidMount() {
+        firebasePlayers.once('value').then( (snapshot) => {
+            const players = firebaseLooper(snapshot);
+
+            this.setState({
+                isLoading: false,
+                players: reverseArray(players)
+            })
+        } )
+    }
+
+    render() {
+        return (
+            <AdminLayout>
+                <div>
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>First name</TableCell>
+                                    <TableCell>Last name</TableCell>
+                                    <TableCell>Number</TableCell>
+                                    <TableCell>Position</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                               {this.state.players ?
+                                    this.state.players.map( (player,i) =>(
+                                        <TableRow key={i}>
+                                            <TableCell >
+                                                <Link to={`/admin_players/add_players/${player.id}`} >
+                                                    {player.name}
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell style={{color: 'red'}}>
+                                                <Link to={`/admin_players/add_players/${player.id}`} >
+                                                    {player.lastname}
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell >
+                                                {player.number}
+                                            </TableCell>
+                                            <TableCell style={{color: 'green'}}>
+                                                {player.position}
+                                            </TableCell>
+                                        </TableRow>
+                                    ) )
+                                    :""
+
+                               }
+                            </TableBody>
+                        </Table>
+                    </Paper>
+
+                    <div className="admin_progress">
+                    {this.state.isLoading ?
+                         <CircularProgress thickness={6} style={{color: "rgba(187, 59, 59, 0.966)"}} />
+                         :''
+                    }
+                    </div>
+                </div>
+                
+            </AdminLayout>
+        );
+    }
+}
+
+export default AdminPlayers;
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
-import 'firebase/auth'
+import 'firebase/auth';
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBWybZdefIE5oLKg55Xoyg5fOPcppj101k",
@@ -19,9 +20,14 @@ const firebaseDB = firebase.database();
 
 const firebaseMatches = firebaseDB.ref('matches');
 const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebasePlayers = firebaseDB.ref('players');
 
 export {
     firebase,
     firebaseMatches,
-    firebasePromotions
+    firebasePromotions,
+    firebaseTeams,
+    firebasePlayers,
+    firebaseDB
 }
